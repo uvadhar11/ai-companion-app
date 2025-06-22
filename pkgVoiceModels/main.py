c5560f9e1c8f83cbddff52387ca3fc2589ca2dd1
Nicholas <nicholaseovva@csus.edu>
2025-06-21 20:04:24 -0700

    make calls using vapi

---
+++ pkgVoiceModels/main.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from vapi import Vapi 
+import voice_setup
+
+load_dotenv() 
+
+VAPI_API_KEY = os.getenv("VAPI_TOKEN")
+VAPI_PHONE_ID = os.getenv("PHONE_ID")
+PERMENANT_ID = os.getenv("PERMANENT_ID")
+ASSISTANT_NAME = "Customer Support Assistant"
+
+if VAPI_API_KEY is None:
+    raise ValueError("VAPI_TOKEN environment variable not set. Check your .env file.")
+elif VAPI_PHONE_ID is None:
+    raise ValueError("PHONE_ID environment variable not set. Check your .env file.")
+else:   
+    vapi_client = Vapi(token=VAPI_API_KEY)
+
+
+voice_agents = {"Dad": "chris", "Mom": "jennifer", "Friend": "friend"}
+persona = voice_agents["Dad"]
+
+convo_context = voice_setup.change_convo_context(persona)  #changes conversation context based on voice_agent.
+
+safe_phrase = "Do you want to get pizza?"
+phone_number = "+12799778354"
+
+
+
+if __name__ == "__main__":
+    existing_id = voice_setup.get_existing_agent(vapi_client, PERMENANT_ID)
+
+    voice_assistant = voice_setup.update_agent(convo_context, persona, vapi_client, existing_id, safe_phrase, phone_number)
+    #voice_assistant = voice_setup.update_call_transfer_number(vapi_client, existing_id, phone_number)
---
+++ pkgVoiceModels/voice_setup.py
@@ -0,0 +1,55 @@
+import conversation_contexts
+
+def get_existing_agent(vapi_client, PERMANENT_ID):
+    assistant_id_to_use = None
+    
+    # Check if a permanent assistant ID is defined
+    if PERMANENT_ID:
+            # Attempt to retrieve the existing assistant
+            existing_assistant = vapi_client.assistants.get(PERMANENT_ID)
+            print(f"Found existing assistant with ID: {existing_assistant.id}. Using existing ID")
+            assistant_id_to_use = existing_assistant.id
+            return assistant_id_to_use
+
+def change_convo_context(selected_persona):
+    convo_context_to_use = conversation_contexts.PERSONAS.get(
+    selected_persona.lower(), # Use .lower() for case-insensitive lookup
+    )
+    return convo_context_to_use
+
+def update_agent(convo_context, voice_agent, vapi_client, existing_id, safe_phrase, phone_number):
+    updated_assistant_config = {
+        "model": { 
+            "provider": "openai",
+            "model": "gpt-4o",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": f"""{convo_context} if you hear {safe_phrase} use the 'transferCall' tool """ 
+                }
+            ], 
+            "tools": [ 
+                {
+                    "type": "transferCall", 
+                    "destinations": [
+                        {"type": "number", "number": phone_number}
+                    ]
+                }
+            ]
+        },
+        
+        "voice": { 
+            "provider": "playht",
+            "voice_id": voice_agent,
+        },
+    
+        "first_message": "Hey are you on your way home, where are you?", 
+
+    }
+    
+    voice_assistant = vapi_client.assistants.update(
+        existing_id,
+        **updated_assistant_config # Unpack the dictionary of changes
+    )
+    print(f"Assistant {voice_assistant.id} updated successfully.") # <--- MOVED TO EXECUTE
+    return voice_assistant # <--- Return after printing
